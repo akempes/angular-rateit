c91e2ba56d908fae1a72ad7865fd19bc282af40d
Arjan Kempes <arjankempes@Arjans-MacBook-Pro.local>
2015-07-21 18:03:34 +0200

    examples added

---
+++ example.js
@@ -0,0 +1,70 @@
+/*global alert, confirm*/
+
+var myApp = angular.module('exampleApp',['ngRateIt']);
+
+myApp.controller('ExampleController', ['$scope', '$q', '$timeout', function($scope, $q, $timeout){
+    'use strict';
+
+        $scope.model = {
+            basic: 0,
+            readonly: 2.5,
+            minMaxStep:1,
+            ispreset: 3,
+            resetable: 1,
+            heightWidth: 1.5,
+            callbacks: 5,
+            custom: 4,
+        };
+
+        $scope.ratedCallback = function () {
+            alert('The rated value is: '+$scope.model.callbacks);
+            console.log('The rated value is: '+$scope.model.callbacks);
+        };
+
+        $scope.resetCallback = function () {
+            alert('Reset clicked!');
+            console.log('Reset clicked!');
+        };
+
+        $scope.overCallback = function (e) {
+            console.log('overCallback', e);
+        };
+
+        $scope.confirmReset = function () {
+            var d = $q.defer(); 
+            if(confirm('Are you sure about resetting this rating?')){
+                d.resolve();
+            }else{
+                d.reject();
+            }
+            return d.promise;
+        };
+
+        $scope.confirmRating = function (newRating) {
+            var d = $q.defer(); 
+
+            $timeout(function  () {
+                if(confirm('Are you sure about rating us with '+newRating+' stars?')){
+                    d.resolve();
+                }else{
+                    d.reject();
+                }
+            });
+            
+            return d.promise;
+        };
+
+        $scope.confirmReset = function () {
+            var d = $q.defer(); 
+            if(confirm('Are you sure about resetting this rating?')){
+                d.resolve();
+            }else{
+                d.reject();
+            }
+            return d.promise;
+        };
+
+    }
+]);
+
+
